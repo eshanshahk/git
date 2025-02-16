597d0253cf7839b2ed8f2944d8ba79868927b4f2
eshanshahk <eshanshahk123@gmail.com>
2025-02-16 10:10:02 +0530

    created function_01

--- function_01.c
+++ function_01.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sum(int x, int y);
+
+int sum(int x, int y){
+    printf("The sum is %d",x+y);
+    return x+y;
+}
 int main() {
-    
+    int a,b;
+    printf("Enter two numbers to add \t");
+    scanf("%d%d",&a,&b);
+    sum(a,b);
     return 0;
 }
