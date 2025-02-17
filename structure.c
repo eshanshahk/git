d72193d6fe9d6016bb7bd69c268d27bc84b1a8d3
eshanshahk <eshanshahk123@gmail.com>
2025-02-17 19:44:58 +0530

    created structure program of one student

---
+++ structure.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+struct student
+{
+    int r_no;
+    char name[20];
+    float mark[3];
+};
+int main()
+{
+    struct student s;
+    int i;
+    float sum=0;
+    printf("Enter your name & roll no \n");
+    scanf("%s %d", s.name, &s.r_no);
+    printf("Enter your marks \n");
+    for (i = 0; i < 3; i++)
+    {
+        scanf("%f", &s.mark[i]);
+    }
+    for (i = 0; i < 3; i++)
+    {
+        sum = sum + s.mark[i];
+    }
+    printf("Student Details are : \n");
+    printf("Name =%s \t Rollno=%d \t Total Mark=%f",s.name,s.r_no,sum);
+    return 0;
+}
