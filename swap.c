51c99310d579668a140d3be8135cc6cbd1b681a1
eshanshahk <eshanshahk123@gmail.com>
2025-04-01 21:01:02 +0530

    update swap.c

--- swap.c
+++ swap.c
@@ -1,16 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void swap(int *x,int *y){
-int **temp;
-  temp=&x;
-  *x=*b;
-  *b=*temp;
-  printf("AFTER SWAPPING A=%d \t B=%d,x,y");
+void swap(int *x, int *y)
+{
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+  printf("AFTER SWAPPING A=%d \t B=%d \n", *x, *y);
 }
 int main()
 {
-int a=5,b=6;
-  printf("BEFORE SWAPPING A="%d" \t B="%d",a,b);
-  swap(&a,&b);
+  int a = 5, b = 6;
+  printf("BEFORE SWAPPING A=%d \t B=%d \n", a, b);
+  swap(&a, &b);
   return 0;
 }
